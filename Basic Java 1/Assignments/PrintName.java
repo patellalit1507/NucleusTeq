e57a995a9cf9a202e03c8f05f720cafa6c08331c
lalit patel <69241759+patellalit1507@users.noreply.github.com>
2023-03-15 23:33:10 +0530

    Update PrintName.java

--- Basic Java 1/Assignments/PrintName.java
+++ Basic Java 1/Assignments/PrintName.java
@@ -1,6 +1,6 @@
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("My name is Madhur Jatiya");
+        System.out.println("My name is Lalit Patel");
     }
 }
