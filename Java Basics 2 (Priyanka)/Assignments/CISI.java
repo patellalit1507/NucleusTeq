4231062589fbab3e5d711b308fd9edebe5d6a3fc
lalit patel <69241759+patellalit1507@users.noreply.github.com>
2023-03-16 00:27:13 +0530

    Update CISI.java

--- Java Basics 2 (Priyanka)/Assignments/CISI.java
+++ Java Basics 2 (Priyanka)/Assignments/CISI.java
@@ -3,16 +3,16 @@ public class CISI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter P : ");
-        double p = sc.nextDouble();
+        double principle = sc.nextDouble();
         System.out.print("Enter R : ");
-        double r = sc.nextDouble();
+        double rate = sc.nextDouble();
         System.out.print("Enter T : ");
-        double t = sc.nextDouble();
+        double time = sc.nextDouble();
         
-        double SI = (p*r*t)/100;
+        double SI = (principle*rate*time)/100;
         System.out.println("Simple Interest = " + SI);
         
-        double compound_interest = p * (Math.pow((1 + r / 100), t)) - p;
+        double compound_interest = principle * (Math.pow((1 + rate / 100), time)) - principle;
         System.out.println("Compound Interest = " + compound_interest);
     }
 }
