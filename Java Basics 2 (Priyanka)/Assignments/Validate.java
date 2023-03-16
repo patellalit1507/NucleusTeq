432c9cc4f30af63dd4fe3e0640ca679afe5223f8
lalit patel <69241759+patellalit1507@users.noreply.github.com>
2023-03-16 10:09:11 +0530

    Update Validate.java

--- Java Basics 2 (Priyanka)/Assignments/Validate.java
+++ Java Basics 2 (Priyanka)/Assignments/Validate.java
@@ -1,19 +1,38 @@
-import java.util.*;
 import java.util.regex.*;
-public class Validate
-{
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        Main m = new Main();
-        if(m.validate(num))
-            System.out.println("Valid");
-        else
-        System.out.println("Not Valid");
-	}
-	
-	public boolean validate(int num)
-	{
-	    return Pattern.matches("^[1-9]{1}[0-9]{2}\\s{0, 1}[0-9]{3}$",Integer.toString(num));
-	}
+ 
+class Validate {
+ 
+    public static boolean isValidCode(String pinCode)
+    {
+ 
+        String regex
+            = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
+        Pattern p = Pattern.compile(regex);
+
+        if (pinCode == null) {
+            return false;
+        }
+	    
+        Matcher m = p.matcher(pinCode);
+ 
+        return m.matches();
+    }
+ 
+    // Driver Code.
+    public static void main(String args[])
+    {
+ 
+    
+        String num1 = "451113";
+        System.out.println(
+            num1 + ": "
+            + isValidCode(num1));
+ 
+  
+        String num2 = "431 001";
+        System.out.println(
+            num2 + ": "
+            + isValidCode(num2));
+ 
+    }
 }
