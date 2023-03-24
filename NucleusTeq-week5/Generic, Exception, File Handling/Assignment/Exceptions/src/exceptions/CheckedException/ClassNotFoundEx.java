e8da245509115ddef5aaa870b09bc373a955c9d9
lalit patel <69241759+patellalit1507@users.noreply.github.com>
2023-03-24 17:24:10 +0530

    Update ClassNotFoundEx.java

--- NucleusTeq-week5/Generic, Exception, File Handling/Assignment/Exceptions/src/exceptions/CheckedException/ClassNotFoundEx.java
+++ NucleusTeq-week5/Generic, Exception, File Handling/Assignment/Exceptions/src/exceptions/CheckedException/ClassNotFoundEx.java
@@ -4,7 +4,7 @@ public class ClassNotFoundEx {
 
     public static void main(String[] args) {
         try {
-            Class.forName("exceptions.UncheckedExeption.madhur");
+            Class.forName("exceptions.UncheckedExeption.lalit");
         } catch (ClassNotFoundException e) {
             System.out.println(e);
         }
